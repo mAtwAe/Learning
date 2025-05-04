9d251d6790e45173407e8d044b9569eb66a35588
ridhuan <mohdridhuan95@gmail.com>
2025-05-04 14:17:31 +0800

    reference

--- deploying/app.py
+++ deploying/app.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    return "<h1>Hello, Docker boiii !!! </h1>"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+      app.run(host='0.0.0.0', port=8080)
